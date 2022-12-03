1450e56bbf4c2c61201b43ba04d1f72d2a3b1daf
Be-Asia McKerracher <amerigirl29@gmail.com>
2022-12-03 14:00:49 -0600

    almost got the database connected!

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,6 +1,18 @@
-﻿namespace BulkyBookWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public String Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+
+        //current date and time will always be assigned to CreatedDateTime
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
