f2102eca193096341fa9e7ce7819c55549a6cfce
Be-Asia McKerracher <amerigirl29@gmail.com>
2022-12-07 09:47:26 -0600

    getting ready for the next step--bootstrapping

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -1,6 +1,6 @@
 ﻿using BulkyBookWeb.Models;
 using Microsoft.EntityFrameworkCore;
-using Artist = BulkyBookWeb.Models.Artist;
+
 
 namespace BulkyBookWeb.Data
 {
@@ -14,6 +14,5 @@ namespace BulkyBookWeb.Data
 
 		public DbSet<Category> Categories { get; set; }
 
-		public DbSet<Artist> Artists { get; set; }
 	}
 }
